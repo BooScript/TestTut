a9aa052be29b612b17de29e1da4953487ab73769
TomChambrier <tomw08@gmail.com>
2015-09-07 08:03:52 +0100

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,81 +1,35 @@
-// scrapes a given profile
-
-var request = require("request"),
-    cheerio = require("cheerio"),
-    queryUrl = "https://www.lendwithcare.org/entrepreneurs/index/13446";
-
-    var baseURL = 'https://www.lendwithcare.org/';
-
-
-function profileScrape(queryUrl) {
-
-// function that takes an array of project urls to query and returns object of all projects attributes
-//function getKeyAttributes(queryUrlArr){}
-    request(queryUrl, function (error, response, body) {
-        if (!error) {
-            var $ = cheerio.load(body);
-
-            //initialise empty array to store key project attributes
-            var keyAttributes = [];
-            $('h1').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-            $('label').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-
-
-            $('p').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-
-            $('span.time').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-            $('div.gm-style').each(function () {
-                keyAttributes.push(this.children.data);
-            });
-
-            // loop for each h3 element on page and push key attributes to array
-            $('h3').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-
-            $('.span').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-
-            $('td').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-
-            ('').each(function () {
-                keyAttributes.push(this.children[0].data);
-            });
-
-
-
-            console.log(keyAttributes);
-        }
-        if (error) {
-            console.log('http request failed to give response');
-        }
-
-        return keyAttributes;
-    });
-
-}
-
+// import modules
+require('./ProjectClass.js');
+var p = require('./profileScrape.js');
+// import function to scrape profile
 var GetProfileLinks = require('./GetProfileLinks_FromDump.js');
 
+// all profile urls consist of this url
+var baseURL = 'https://www.lendwithcare.org/';
 
+//test projattr class is exposed
+console.log( projectAttr);
 
+// get array of funded project profile links
 var linksArr = GetProfileLinks.getProfileLinks;
 
+//array to hold project data
+var profileDataRaw = [];
 
-for (var i = 0; i < 1; i++){
+//scrape profiles
+for (var i = 0; i < 50; i++){
    var urlCur = baseURL + linksArr[i];
     console.log(urlCur);
-    console.log(profileScrape(urlCur) + ' i is ' + i);
+    profileDataRaw.push(p.profileScrape(urlCur) + ' i is ' + i);
+    console.log(profileDataRaw[i])
 }
+
+// array to hold individual profile data
+varprofilesArr = [];
+
+//loop through
+
+// write key project attributes to JSON file for completed projects
+//fs.write('CompletedProjects.json', JSON.stringify(), 'w');
+
 
