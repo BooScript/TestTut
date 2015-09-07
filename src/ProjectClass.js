2702efb472df935910660d866f84dcefc9ee568e
TomChambrier <tomw08@gmail.com>
2015-09-07 07:30:47 +0100

    Create ProjectClass.js
    class to store individual project attributes

---
+++ src/ProjectClass.js
@@ -0,0 +1,49 @@
+/**
+ * Created by Tom on 07/09/2015.
+ */
+// class to stor individual project attributes
+ projectAttr = {
+        name: null,
+        dateLoanAdded: null,
+        value: null,
+        repaymentTerm: null,
+        activityType: null,
+        mfiPartner: null,
+        description: null,
+        expectedPayments: {
+            month1: {
+                date: null,
+                value: null
+            },
+            month2: {
+                date: null,
+                value: null
+            },
+            month3: {
+                date: null,
+                value: null
+            },
+            month4: {
+                date: null,
+                value: null
+            }
+        },
+        actualRepayments: {
+            month1: {
+                date: null,
+                value: null
+            },
+            month2: {
+                date: null,
+                value: null
+            },
+            month3: {
+                date: null,
+                value: null
+            },
+            month4: {
+                date: null,
+                value: null
+            }
+        }
+    };
